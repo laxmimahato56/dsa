e4115c45840b7f306af70ce0097258173dba2b9c
Laxmi Mahato <laxmi.mahato.lm.9803@gmail.com>
2025-05-03 09:52:00 +0545

    factorial

---
+++ factorial.js
@@ -0,0 +1,16 @@
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(3)); // 6
+console.log(factorial(4)); // 24
+console.log(factorial(5)); // 120
+
+// Big-O = O(n)
