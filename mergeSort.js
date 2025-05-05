6e31b75ed3a3f5aeb2f86be1696d69432bd2c07f
Laxmi Mahato <laxmi.mahato.lm.9803@gmail.com>
2025-05-05 21:57:00 +0545

    merge sort

---
+++ mergeSort.js
@@ -0,0 +1,24 @@
+function mergeSort(arr) {
+  if (arr.length < 2) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const leftArray = arr.slice(0, mid);
+  const rightArray = arr.slice(mid);
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
+
+function merge(leftArr, rightArr) {
+  const sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+const arr = [8, 20, -2, 4, -6];
+console.log(mergeSort(arr)); // [-6, -2, 4, 8, 20]
+
+// Big-O = O(nlogn)
