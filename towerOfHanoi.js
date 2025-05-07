4ab3c1ea47468c03bee1bc6b162a9d83797c2347
Laxmi Mahato <laxmi.mahato.lm.9803@gmail.com>
2025-05-07 20:51:00 +0545

    tower of hanoi

---
+++ towerOfHanoi.js
@@ -0,0 +1,18 @@
+// shift n-1 disks from A to B using C (when required)
+// shift last disk from A to C
+// shift n-1 disks from B to C using A (when required)
+
+function towerOfHanoi(n, fromRod, toRod, usingRod) {
+  if (n === 1) {
+    console.log(`Move disk 1 from ${fromRod} to ${toRod}`);
+    return;
+  }
+
+  towerOfHanoi(n - 1, fromRod, usingRod, toRod);
+  console.log(`Move disk ${n} from ${fromRod} to ${toRod}`);
+  towerOfHanoi(n - 1, usingRod, toRod, fromRod);
+}
+
+towerOfHanoi(3, "A", "C", "B");
+
+// Big-O = O(2^n)
